911b51ce2009df00b5421fe6c9a8c774c1b7f189
Vesel4ak <68870977+Vesel4akRepositories@users.noreply.github.com>
2024-06-09 19:44:14 +0300

    sprint_08

---
+++ unsplash_practice/ImagesListCell.swift
@@ -0,0 +1,34 @@
+//
+//  ImagesListCell.swift
+//  unsplash_practice
+//
+//  Created by Денис Петров on 09.06.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet private weak var bgImage: UIImageView!
+    @IBOutlet private weak var likeIcon: UIImageView!
+    @IBOutlet private weak var dateLabel: UILabel!
+    
+    func setBgImage(imageName: String){
+        bgImage.image = UIImage(named: imageName)
+    }
+    
+    func setLikeIcon(isLiked: Bool) {
+        likeIcon.image = UIImage(named: isLiked ? "like_active" : "no_like")
+    }
+    
+    func setDate(dateString: String){
+        dateLabel.text = dateString
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 16, left: 16, bottom: 0, right: 16))
+    }
+}
